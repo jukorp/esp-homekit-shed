d46670033171d6cfca4f3770ee091bef66dfa0b9
Alex Taylor <alex.taylor@enlighten.co.nz>
2018-10-23 16:35:03 +1300

    Themostat and pins

--- shed/shed.c
+++ shed/shed.c
@@ -32,12 +32,15 @@ void temperature_sensor_identify(homekit_value_t _value) {
     // }
 }
 
+//west external
 homekit_characteristic_t temperature1 = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
 homekit_characteristic_t humidity1    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
 
+//east external
 homekit_characteristic_t temperature2 = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
 homekit_characteristic_t humidity2    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
 
+// interior
 homekit_characteristic_t temperature3 = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
 homekit_characteristic_t humidity3    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
 
@@ -50,7 +53,62 @@ homekit_characteristic_t humidity3    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE
 // 9.22 temperature sensor 
 // 9.25 battery serrvice
 
-void read_temperature_sensor(int pin, homekit_characteristic_t temperature, homekit_characteristic_t humidity) {
+
+homekit_characteristic_t thermostat_current_state = HOMEKIT_CHARACTERISTIC_(CURRENT_HEATING_COOLING_STATE, 0);
+homekit_characteristic_t thermostat_target_state = HOMEKIT_CHARACTERISTIC_(TARGET_HEATING_COOLING_STATE, 0);
+homekit_characteristic_t thermostat_target_temperature = HOMEKIT_CHARACTERISTIC_(TARGET_TEMPERATURE, 18.0f);
+
+bool east_west_fan_current = false;
+bool west_east_fan_current = false;
+
+homekit_value_t east_west_fan_get() {
+    return HOMEKIT_BOOL(east_west_fan_current);
+}
+
+void east_west_fan_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    east_west_fan_current = value.bool_value;
+    gpio_write(EASTWEST_FAN, east_west_fan_current ? 1 : 0);
+}
+
+homekit_value_t west_east_fan_get() {
+    return HOMEKIT_BOOL(west_east_fan_current);
+}
+
+void west_east_fan_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    west_east_fan_current = value.bool_value;
+    gpio_write(WESTEAST_FAN, west_east_fan_current ? 1 : 0);
+}
+
+homekit_characteristic_t east_west_fan_state = HOMEKIT_CHARACTERISTIC_(ON, false,
+                .getter=east_west_fan_get,
+                .setter=east_west_fan_set
+            );
+homekit_characteristic_t west_east_fan_state = HOMEKIT_CHARACTERISTIC_(ON, false,
+                .getter=west_east_fan_get,
+                .setter=west_east_fan_set
+            );
+
+/* 
+    //  Required Characteristics:
+    - CURRENT_HEATING_COOLING_STATE
+    - TARGET_HEATING_COOLING_STATE
+    - CURRENT_TEMPERATURE
+    - TARGET_TEMPERATURE
+    - TEMPERATURE_DISPLAY_UNITS
+ */
+
+
+void read_temperature_sensor(int pin, homekit_characteristic_t* temperature, homekit_characteristic_t* humidity) {
     float humidity_value, temperature_value;
 
     bool success = dht_read_float_data(
@@ -59,11 +117,11 @@ void read_temperature_sensor(int pin, homekit_characteristic_t temperature, home
     );
     
     if (success) {
-        temperature.value.float_value = temperature_value;
-        humidity.value.float_value = humidity_value;
+        temperature->value.float_value = temperature_value;
+        humidity->value.float_value = humidity_value;
 
-        homekit_characteristic_notify(&temperature, HOMEKIT_FLOAT(temperature_value));
-        homekit_characteristic_notify(&humidity, HOMEKIT_FLOAT(humidity_value));
+        homekit_characteristic_notify(temperature, HOMEKIT_FLOAT(temperature_value));
+        homekit_characteristic_notify(humidity, HOMEKIT_FLOAT(humidity_value));
 
         printf("Current temperature: ");
         printf("%.2f",temperature_value);
@@ -80,17 +138,143 @@ void temperature_sensor_task(void *_args) {
 
     gpio_enable(EXTERIOR_FLOOD, GPIO_OUTPUT);
     gpio_enable(INTERIOR_LIGHTS, GPIO_OUTPUT);
-    gpio_enable(PHONE_CHARGER, GPIO_OUTPUT);
+    //gpio_enable(PHONE_CHARGER, GPIO_OUTPUT);
     gpio_enable(EASTWEST_FAN, GPIO_OUTPUT);
     gpio_enable(WESTEAST_FAN, GPIO_OUTPUT);
 
+    bool west_east_target = false;
+    bool east_west_target = false;
+
     while (1) {
-        read_temperature_sensor(SENSOR1_PIN, temperature1, humidity1);
+        read_temperature_sensor(SENSOR1_PIN, &temperature1, &humidity1);
         vTaskDelay(100 / portTICK_PERIOD_MS);
-        read_temperature_sensor(SENSOR2_PIN, temperature2, humidity2);
+        read_temperature_sensor(SENSOR2_PIN, &temperature2, &humidity2);
         vTaskDelay(100 / portTICK_PERIOD_MS);
-        read_temperature_sensor(SENSOR3_PIN, temperature3, humidity3);
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        read_temperature_sensor(SENSOR3_PIN, &temperature3, &humidity3);
+
+        float west = temperature1.value.float_value;
+        float east = temperature2.value.float_value;
+        float internal = temperature3.value.float_value;
+        float target = thermostat_target_temperature.value.float_value;
+        float mode = thermostat_target_state.value.int_value;
+        float cooling = mode == 2;
+        float heating = mode == 1;
+        float automatic = mode == 3;
+        float threshold = 0.5f;
+
+        printf("cooling=%s heating=%s automatic=%s internal=%.3f east=%.3f west=%.3f target=%.3f\n", 
+            cooling ? "true" : "false", 
+            heating ? "true" : "false", 
+            automatic ? "true" : "false", 
+            internal,
+            east,
+            west,
+            target);         
+
+        // inside > target && (cooling || auto)
+            // if east < inside
+                // east_west fans on
+                // west_east fans off
+            // else if west < inside
+                // west_east fans on 
+                // east_west fans off
+            // else
+                // west_east fans off
+                // east_west fans off
+        // inside < target && (heating || auto)
+            // if east > inside
+                // east_west fans on
+                // west_east fans off
+            // else if west > inside
+                // west_east fans on 
+                // east_west fans off
+            // else
+                // west_east fans off
+                // east_west fans off
+        if (internal > target + threshold && (cooling || automatic))
+        {
+            if (east < internal)
+            {
+                east_west_target = true;
+                west_east_target = false;
+
+                mode = 2;
+            }
+            else if (west < internal)
+            {
+                east_west_target = false;
+                west_east_target = true;
+
+                mode = 2;
+            }
+            else
+            {
+                east_west_target = false;
+                west_east_target = false;
+
+                mode = 0;
+            }
+        }  
+        else if (internal < target - threshold && (heating || automatic))
+        {
+            if (east > internal)
+            {
+                east_west_target = true;
+                west_east_target = false;
+
+                mode = 1;
+            }
+            else if (west > internal)
+            {
+                east_west_target = false;
+                west_east_target = true;
+
+                mode = 1;
+            }
+            else
+            {
+                east_west_target = false;
+                west_east_target = false;
+
+                mode = 0;
+            }
+        }
+        else
+        {
+            east_west_target = false;
+            west_east_target = false;
+            
+            mode = 0;
+        }
+
+        if (east_west_fan_current != east_west_target)
+        {
+            east_west_fan_current = east_west_target;
+            east_west_fan_state.value.bool_value = east_west_target;
+            homekit_characteristic_notify(&east_west_fan_state, HOMEKIT_BOOL(east_west_target));
+        }
+            
+        if (west_east_fan_current != west_east_target)
+        {
+            west_east_fan_current = west_east_target;
+            west_east_fan_state.value.bool_value = west_east_target;
+            homekit_characteristic_notify(&west_east_fan_state, HOMEKIT_BOOL(west_east_target));
+        }
+
+        gpio_write(EASTWEST_FAN, east_west_target ? 1 : 0);
+        gpio_write(WESTEAST_FAN, west_east_target ? 1 : 0);
+        if (thermostat_current_state.value.int_value != mode)
+        {
+            thermostat_current_state.value.int_value = mode;
+            homekit_characteristic_notify(&thermostat_current_state, HOMEKIT_UINT8(mode));
+        }
+
+        printf("current=%s fans_east=%s fans_west=%s\n", 
+            mode == 0 ? "off" : (mode == 1 ? "heat" : (mode == 2 ? "cool" : "--")),
+            east_west_target ? "on" : "off",
+            west_east_target ? "on" : "off");         
+
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -135,17 +319,6 @@ void exterior_flood_set(homekit_value_t value) {
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Shed Control");
 homekit_characteristic_t serialNumber = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, "XXXXXXXXXX");
 
-homekit_characteristic_t thermostat_current_state = HOMEKIT_CHARACTERISTIC_(CURRENT_HEATING_COOLING_STATE, 0);
-homekit_characteristic_t thermostat_target_state = HOMEKIT_CHARACTERISTIC_(TARGET_HEATING_COOLING_STATE, 0);
-homekit_characteristic_t thermostat_target_temperature = HOMEKIT_CHARACTERISTIC_(TARGET_TEMPERATURE, 18.0f);
-/* 
-    //  Required Characteristics:
-    - CURRENT_HEATING_COOLING_STATE
-    - TARGET_HEATING_COOLING_STATE
-    - CURRENT_TEMPERATURE
-    - TARGET_TEMPERATURE
-    - TEMPERATURE_DISPLAY_UNITS
- */
 
 homekit_characteristic_t button = HOMEKIT_CHARACTERISTIC_(PROGRAMMABLE_SWITCH_EVENT, 0);
 homekit_characteristic_t motion = HOMEKIT_CHARACTERISTIC_(MOTION_DETECTED, false);
@@ -174,6 +347,19 @@ void button_callback(uint8_t gpio, button_event_t event) {
     }
 }
 
+void motion_sensor_callback(uint8_t gpio) {
+    
+    if (gpio == MOTION_SENSOR_PIN){
+        int new = 0;
+        new = gpio_read(MOTION_SENSOR_PIN);
+        motion.value = HOMEKIT_BOOL(new);
+        homekit_characteristic_notify(&motion, HOMEKIT_BOOL(new));
+        printf("Motion Detected on %d\r\n", gpio);
+    }
+    else {
+        printf("Interrupt on %d\r\n", gpio);
+    }
+}
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_garage, .services=(homekit_service_t*[]) {
@@ -232,6 +418,18 @@ homekit_accessory_t *accessories[] = {
             NULL
         }),
 
+        HOMEKIT_SERVICE(FAN, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "East to west fans"),
+            &east_west_fan_state,
+            NULL
+        }),
+
+        HOMEKIT_SERVICE(FAN, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "West to east fans"),
+            &west_east_fan_state,
+            NULL
+        }),
+
 // INTERIOR LIGHTS
         HOMEKIT_SERVICE(STATELESS_PROGRAMMABLE_SWITCH, .characteristics=(homekit_characteristic_t*[]) {
             HOMEKIT_CHARACTERISTIC(NAME, "Light Switch"),
@@ -270,6 +468,7 @@ homekit_server_config_t config = {
     .accessories = accessories,
     .password = "506-81-392"
 };
+
 
 void on_wifi_ready() {
     homekit_server_init(&config);
@@ -386,5 +585,10 @@ void user_init(void) {
     if (button_create(BUTTON_PIN, button_callback)) {
         printf("Failed to initialize button\n");
     }
+
+    gpio_enable(MOTION_SENSOR_PIN, GPIO_INPUT);
+    gpio_set_pullup(MOTION_SENSOR_PIN, false, false);
+    gpio_set_interrupt(MOTION_SENSOR_PIN, GPIO_INTTYPE_EDGE_ANY, motion_sensor_callback);
+
 }
 
